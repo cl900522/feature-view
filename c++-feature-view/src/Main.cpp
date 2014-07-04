f3a4da4a45afa084532705eea3130f0b27c00b66
Acme-CMX <545435711@qq.com>
2014-07-04 20:34:38 +0800

    c++ 初始化

---
+++ c++-feature-view/src/Main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: 2014年7月4日
+ *      Author: 明轩
+ */
+
+#include "Main.h"
+
+namespace acme {
+
+Main::Main() {
+    // TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace acme */
---
+++ c++-feature-view/src/Main.h
@@ -0,0 +1,21 @@
+/*
+ * Main.h
+ *
+ *  Created on: 2014年7月4日
+ *      Author: 明轩
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+namespace acme {
+
+class Main {
+public:
+    Main();
+    virtual ~Main();
+};
+
+} /* namespace acme */
+
+#endif /* MAIN_H_ */
