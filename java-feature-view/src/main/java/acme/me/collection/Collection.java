d9b3b42c8db0f2e2139c053b61b466df3b402654
cl900522 <545435711@qq.com>
2014-07-04 18:02:22 +0800

    添加注释

--- java-feature-view/src/main/java/acme/me/collection/Collection.java
+++ java-feature-view/src/main/java/acme/me/collection/Collection.java
@@ -3,6 +3,9 @@ package acme.me.collection;
 import java.util.*;
 
 public class Collection {
+    /**
+     * 根据数据量的大小，可以设置初始化时候，collection的size
+     */
     List<String> list= new ArrayList<String>(6);
     Map<String,Object> map = new LinkedHashMap<String,Object>(100);
 }
