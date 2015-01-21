77d28c12e9a32b3edb2f1ab29d5c593abc382f67
chenmx <mingxuan.chen@sipingsoft.com>
2015-01-21 15:43:21 +0800

    增加base64对文件的编码和解码功能

---
+++ java-feature-view/src/main/java/acme/me/basic/Base64View.java
@@ -0,0 +1,72 @@
+package acme.me.basic;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class Base64View {
+    public String encodeImgageToBase64(URL imageUrl) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        ByteArrayOutputStream outputStream = null;
+        try {
+            BufferedImage bufferedImage = ImageIO.read(imageUrl);
+            outputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "jpg", outputStream);
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(outputStream.toByteArray());// 返回Base64编码过的字节数组字符串
+    }
+
+    public String encodeImgageToBase64(File imageFile) {// 将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        ByteArrayOutputStream outputStream = null;
+        try {
+            BufferedImage bufferedImage = ImageIO.read(imageFile);
+            outputStream = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, "png", outputStream);
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(outputStream.toByteArray());// 返回Base64编码过的字节数组字符串
+    }
+
+    public void decodeBase64ToImage(String base64, String path) {
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            FileOutputStream write = new FileOutputStream(new File(path));
+            byte[] decoderBytes = decoder.decodeBuffer(base64);
+            write.write(decoderBytes);
+            write.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        Base64View view = new Base64View();
+        String filePath = ClassLoader.getSystemClassLoader().getResource("base64.png").getPath();
+        File imageFile = new File(filePath);
+        if(!imageFile.exists()){
+            return;
+        }
+        String codeString = view.encodeImgageToBase64(imageFile);
+        System.out.println(codeString);
+        view.decodeBase64ToImage(codeString, filePath+"_decode");
+    }
+}
