72c6323526ff3a85b22edc64e3978cc1cd0e2b72
chenmx <mingxuan.chen@sipingsoft.com>
2015-02-09 10:51:09 +0800

    android edit and spring edit

--- c++-feature-view/src/Init.cpp
+++ c++-feature-view/src/Init.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 int main() {
-    cout << "Hello C++" << endl;
-    return 0;
+    for(int i=0; i<100; i++){
+        unsigned seed = (unsigned) time(NULL);
+        printf("Seed:%d", seed);
+        srand(seed);
+        printf("Result:%d\n",rand());
+    }
 }
