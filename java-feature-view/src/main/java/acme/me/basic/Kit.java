2214037e55cb454cea6030f4806c289622b91e24
cl900522 <545435711@qq.com>
2015-04-23 22:15:24 +0800

    java技巧

---
+++ java-feature-view/src/main/java/acme/me/basic/Kit.java
@@ -0,0 +1,20 @@
+package acme.me.basic;
+
+public class Kit {
+    public static void main(String[] args) {
+        int seed = 0;
+        for (int i = 0; i < 10; i++) {
+            seed = seed++;
+        }
+        System.out.println("Ned value is still:" + seed);
+
+        int j = 80;
+        String s1 = String.valueOf(j < 100 ? j : 100.0);
+        String s2 = String.valueOf(j < 100 ? j : 100);
+        if (s1.equals(s2)) {
+            System.out.println("S1 and S2 is the same!");
+        } else {
+            System.out.println("S1 and S2 is NOT the same!");
+        }
+    }
+}
