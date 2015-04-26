f66dedc6a6bcddff1cf4ec50c81451187e54edc9
cl900522 <545435711@qq.com>
2015-04-26 21:24:44 +0800

    javascript object view

--- ui-feature-view/WebContent/js/javascript-array.js
+++ ui-feature-view/WebContent/js/javascript-array.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    var array = ["red", "green", "black", "yellow", "brown"];
+    console.log("############### Array convert by join ',' & '||'");
+    console.log(array.join(","));
+    console.log(array.join("||"));
+    console.log();
+    var color = "";
+    
 })
