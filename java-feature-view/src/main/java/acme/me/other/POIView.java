9ca6329b7d8b9599ed0156c6305d5acd15c61ac4
cl900522 <545435711@qq.com>
2015-09-09 22:31:24 +0800

    poi view

---
+++ java-feature-view/src/main/java/acme/me/other/POIView.java
@@ -0,0 +1,134 @@
+package acme.me.other;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.poi.hssf.usermodel.HSSFHeader;
+import org.apache.poi.hssf.usermodel.HeaderFooter;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Footer;
+import org.apache.poi.ss.usermodel.Header;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.WorkbookUtil;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class POIView {
+    public static void main(String[] args) throws IOException{
+        Workbook wb = new XSSFWorkbook();
+
+        /*******************Sheet one***********************/
+        Sheet sheet1 = wb.createSheet("first sheet");
+
+        CreationHelper createHelper = wb.getCreationHelper();
+        Row row = sheet1.createRow((short)1);
+        // Create a cell and put a value in it.
+        Cell cell = row.createCell(0);
+        cell.setCellValue(1);
+
+        // Or do it on one line.
+        row.createCell(1).setCellValue(1.2);
+        row.createCell(2).setCellValue(createHelper.createRichTextString("This is a string"));
+        row.createCell(3).setCellValue(true);
+
+        /*******************Sheet two***********************/
+        Sheet sheet2 = wb.createSheet("second sheet");
+
+        row = sheet2.createRow(0);
+        // Create a cell and put a date value in it.  The first cell is not styled
+        // as a date.
+        cell = row.createCell(0);
+        cell.setCellValue(new Date());
+
+        // we style the second cell as a date (and time).  It is important to
+        // create a new cell style from the workbook otherwise you can end up
+        // modifying the built in style and effecting not only this cell but other cells.
+        CellStyle cellStyle = wb.createCellStyle();
+        cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("m/d/yy h:mm"));
+        cell = row.createCell(1);
+        cell.setCellValue(new Date());
+        cell.setCellStyle(cellStyle);
+
+        //you can also set date as java.util.Calendar
+        cell = row.createCell(2);
+        cell.setCellValue(Calendar.getInstance());
+        cell.setCellStyle(cellStyle);
+
+
+        /*******************Sheet three***********************/
+        String safeName = WorkbookUtil.createSafeSheetName("[O'Brien's sales*?]"); // returns " O'Brien's sales   "
+        Sheet sheet3 = wb.createSheet(safeName);
+
+        row = sheet3.createRow((short)2);
+        row.createCell(0).setCellValue(1.1);
+        row.createCell(1).setCellValue(new Date());
+        row.createCell(2).setCellValue(Calendar.getInstance());
+        row.createCell(3).setCellValue("a string");
+        row.createCell(4).setCellValue(true);
+        row.createCell(5).setCellType(Cell.CELL_TYPE_ERROR);
+
+        /*******************Sheet four***********************/
+        Sheet sheet4 = wb.createSheet();
+        row = sheet4.createRow((short) 2);
+        row.setHeightInPoints(30);
+
+        createCell(wb, row, (short) 0, CellStyle.ALIGN_CENTER, CellStyle.VERTICAL_BOTTOM);
+        createCell(wb, row, (short) 1, CellStyle.ALIGN_CENTER_SELECTION, CellStyle.VERTICAL_BOTTOM);
+        createCell(wb, row, (short) 2, CellStyle.ALIGN_FILL, CellStyle.VERTICAL_CENTER);
+        createCell(wb, row, (short) 3, CellStyle.ALIGN_GENERAL, CellStyle.VERTICAL_CENTER);
+        createCell(wb, row, (short) 4, CellStyle.ALIGN_JUSTIFY, CellStyle.VERTICAL_JUSTIFY);
+        createCell(wb, row, (short) 5, CellStyle.ALIGN_LEFT, CellStyle.VERTICAL_TOP);
+        createCell(wb, row, (short) 6, CellStyle.ALIGN_RIGHT, CellStyle.VERTICAL_TOP);
+
+        /*******************Sheet five***********************/
+        Sheet sheet5 = wb.createSheet("new sheet");
+
+        // Create a row and put some cells in it. Rows are 0 based.
+        row = sheet5.createRow(1);
+
+        // Create a cell and put a value in it.
+        cell = row.createCell(1);
+        cell.setCellValue(4);
+
+        // Style the cell with borders all around.
+        CellStyle style = wb.createCellStyle();
+        style.setBorderBottom(CellStyle.BORDER_THIN);
+        style.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+        style.setBorderLeft(CellStyle.BORDER_THIN);
+        style.setLeftBorderColor(IndexedColors.GREEN.getIndex());
+        style.setBorderRight(CellStyle.BORDER_THIN);
+        style.setRightBorderColor(IndexedColors.BLUE.getIndex());
+        style.setBorderTop(CellStyle.BORDER_MEDIUM_DASHED);
+        style.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        cell.setCellStyle(style);
+
+        Header header = sheet5.getHeader();
+        header.setCenter("Center Header");
+        header.setLeft("Left Header");
+        header.setRight(HSSFHeader.font("Stencil-Normal", "Italic") +
+                        HSSFHeader.fontSize((short) 16) + "Right w/ Stencil-Normal Italic font and size 16");
+
+        Footer footer = sheet5.getFooter();
+        footer.setRight( "Page " + HeaderFooter.page() + " of " + HeaderFooter.numPages() );
+
+        FileOutputStream fileOut = new FileOutputStream("workbook.xlsx");
+        wb.write(fileOut);
+        fileOut.close();
+    }
+
+    private static void createCell(Workbook wb, Row row, short column, short halign, short valign) {
+        Cell cell = row.createCell(column);
+        cell.setCellValue("Align It");
+        CellStyle cellStyle = wb.createCellStyle();
+        cellStyle.setAlignment(halign);
+        cellStyle.setVerticalAlignment(valign);
+        cellStyle.setWrapText(true);
+        cell.setCellStyle(cellStyle);
+    }
+}
