b7c0caf596a308f632c42aee3bba735ee9b5c1a0
cl900522 <545435711@qq.com>
2015-11-05 08:41:11 +0800

    MEMCACHE UPDATE

--- java-feature-view/src/main/java/acme/me/cache/MemcacheView.java
+++ java-feature-view/src/main/java/acme/me/cache/MemcacheView.java
@@ -1,13 +1,16 @@
 package acme.me.cache;
 
+import java.io.Serializable;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import net.spy.memcached.MemcachedClient;
 
 public class MemcacheView {
     public static void main(String[] args) throws Exception {
-        MemcachedClient c = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
+        MemcachedClient c = new MemcachedClient(new InetSocketAddress("192.168.10.101", 11211));
 
         // 存取一个简单的Integer
         // Store a value (async) for one hour
@@ -31,6 +34,32 @@ public class MemcacheView {
         System.out.println(storedUser.userName + " " + storedUser.password + " " + storedUser.birthDate.getTime());
         System.out.println("取之后的时间：" + System.currentTimeMillis());
 
+        Long s = System.currentTimeMillis();
+        List<CacheObject> list = new ArrayList<CacheObject>();
+        for (int i = 0; i < 100000; i++) {
+            CacheObject a = new CacheObject();
+            a.openid = Math.random()+"";
+            list.add(a);
+        }
+
+        c.delete("CACHE_QUEUE");
+        c.add("CACHE_QUEUE", 3600, list);
+
+        list = (List<CacheObject>) c.get("CACHE_QUEUE");
+        System.out.println(list.size());
+        for (CacheObject co : list) {
+            System.out.println(co.toString());
+        }
+        System.out.println(System.currentTimeMillis() - s);
+
         c.shutdown();
+    }
+
+    public static class CacheObject implements Serializable {
+        public String openid;
+
+        public String toString() {
+            return openid;
+        }
     }
 }
