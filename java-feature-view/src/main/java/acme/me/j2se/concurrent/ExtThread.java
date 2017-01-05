23e2c79649b0027a302a94cff069f8c63193909e
cl900522 <545435711@qq.com>
2017-01-05 16:21:06 +0800

    开启线程方式

--- java-feature-view/src/main/java/acme/me/j2se/concurrent/ExtThread.java
+++ java-feature-view/src/main/java/acme/me/j2se/concurrent/ExtThread.java
@@ -1,42 +1,58 @@
 package acme.me.j2se.concurrent;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * 不建议使用此方法定义线程，因为采用继承Thread的方式定义线程后，你不能在继承其他的类了，导致程序的可扩展性大大降低。
  * @author 明轩
- *
  */
 public class ExtThread extends Thread {
     private static Logger logger = LoggerFactory.getLogger(ExtThread.class);
 
     @Override
     public void run() {
-        logger.error("ExtThread "+Thread.currentThread().getName()+" is running!");
-        for(int i=0;i<100;i++){
-            if(i==49)
+        logger.error(Thread.currentThread().getName() + " is running!");
+        for (int i = 0; i < 100; i++) {
+            if (i == 49) {
                 try {
+                    logger.error(Thread.currentThread().getName() + " will sleep");
                     Thread.sleep(10);
+                    logger.error(Thread.currentThread().getName() + " waked up");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            System.out.println("###"+i);
+            }
         }
     }
 
-    public static void main(String[] args){
+    @Test
+    public void extThreadTest() {
+        Thread.currentThread().setName("#Main Tread");
+
+        Thread thread = new ExtThread();
+        thread.setName("$New Thread instance of ExtThread");
+        thread.start();
+        thread.run();
+        System.out.println("All Finish");
+    }
+
+    @Test
+    public void extRunnableTest() {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                exe();
+                logger.error(Thread.currentThread().getName() + " is running!");
             }
         });
-        thread.run();
-        System.out.println("Finish");
-    }
-
-    public static void exe (){
-        System.out.println("Exe function do!");
+        thread.setName("$New Thread instance of Runnable");
+        thread.start();
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("All Finish");
     }
 }
