0064a2ddc3be25cada0262533ee2784231790dfc
cl900522 <545435711@qq.com>
2017-10-21 13:04:19 +0800

    hdfs write function view

--- java-feature-view/src/main/java/acme/me/hadoop/dfs/HDFSTest.java
+++ java-feature-view/src/main/java/acme/me/hadoop/dfs/HDFSTest.java
@@ -5,12 +5,13 @@ import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
- 
+
 
 public class HDFSTest {
     private FileSystem fs = null;
@@ -18,25 +19,34 @@ public class HDFSTest {
     public void init(){
         try {
             Configuration conf = new Configuration();
-            fs = FileSystem.get(URI.create("hdfs://192.168.100.159/"), conf);
+            fs = FileSystem.get(URI.create("hdfs://192.168.100.200/"), conf);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
 
     @Test
     public void readListTest() throws Exception {
         FSDataInputStream fsdis = null;
         try{
-            fsdis = fs.open(new Path("/tmp/.bashrc"));
+            fsdis = fs.open(new Path("/data/about.html"));
             IOUtils.copyBytes(fsdis, System.out, 4091, false);
             fsdis.seek(3);
+            System.out.println("\n");
             IOUtils.copyBytes(fsdis, System.out, 4091, false);
         }finally{
             IOUtils.closeStream(fsdis);
         }
+    }
+
+    @Test
+    private void writeFileTest() throws IOException {
+        fs.createNewFile(new Path("/data/d.txt"));
+        FSDataOutputStream append = fs.append(new Path("/data/d.txt"));
+        append.write("Hello".getBytes());
+        append.close();
     }
 }
