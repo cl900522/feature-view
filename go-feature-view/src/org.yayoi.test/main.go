260f14c8717f49b51d2af1e0211d9873502c705a
cl900522 <545435711@qq.com>
2018-05-28 22:58:45 +0800

    go and spark view

--- go-feature-view/src/org.yayoi.test/main.go
+++ go-feature-view/src/org.yayoi.test/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "org.yayoi.test/variable"
+import (
+	"fmt"
+	"os"
+
+	"org.yayoi.test/variable"
+)
 
 func main() {
 	variable.Print()
+}
+
+func pritArgs() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
