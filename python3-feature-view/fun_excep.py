4246a7806ec38dd3175d1ab78972868a4ab08150
kobe <xiangli@ubuntukylin.com>
2018-08-08 17:21:04 +0800

    add python code

---
+++ python3-feature-view/fun_excep.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again")
+    else:
+        print("Exit, bye!")
+        break
+
+class MyError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+try:
+    raise MyError("Throw an Error")
+except MyError as e:
+    print(e);
+finally:
+    print('Cleaning it')
---
+++ python3-feature-view/oo_Vector.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+class Vector:
+    x=0
+    y=0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        print(self.__class__)
+
+    def __add__(me, other):
+        return Vector(me.x+other.x, me.y+other.y)
+
+    def __str__(self):
+        return 'Vector (%d,%d)' % (self.x, self.y)
+
+    def __gt__(self, other):
+        if (self.x**2 + self.y**2 - other.x**2 - other.y**2 > 0):
+            return 1
+        else:
+            return -1
+
+v1 = Vector(2,2)
+v2 = Vector(1,5)
+v3 = v1+v2
+
+print(v3)
+L = [v1, v2, v3]
+print(v1>v2)
+print(id(v3))
