7f536fcf22b4b3e24d8a944cae48ff1e0035f20c
kobe <xiangli@ubuntukylin.com>
2018-08-10 17:56:49 +0800

    add threding and Lock

---
+++ python3-feature-view/fun_url.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import urllib.request
+import os
+
+tfile = open("./baidu.com.html", "wb")
+
+file=urllib.request.urlopen('http://www.baidu.com')
+data=file.read()    #读取全部
+
+tfile.write(data)
+tfile.close()
+print(1)
