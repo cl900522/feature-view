5b5f7d6df31191e2e7573e543e7668097d319a4e
cl900522 <545435711@qq.com>
2018-10-28 23:25:58 +0800

    重新配置log

--- java-feature-view/common/src/main/java/acme/me/other/ShellTerminalTest.java
+++ java-feature-view/common/src/main/java/acme/me/other/ShellTerminalTest.java
@@ -1,14 +1,19 @@
 package acme.me.other;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class ShellTerminalTest {
+    private static final Logger logger = Logger.getLogger(ShellTerminal.class);
+
     @Test
     public void terminalTest() {
         try {
             ShellTerminal t = new ShellTerminal();
             t.exec("D:");
+            logger.debug("Go d");
             t.exec("cd D:\\04_git");
+            logger.debug("Go 04_git");
             t.exec("echo %PATH%");
             t.close();
             Thread.sleep(3000);
