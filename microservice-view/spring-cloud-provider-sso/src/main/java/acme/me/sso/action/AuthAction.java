be1bc239a9a1a0fae3c6d3455e96bbdd1541c95d
cl900522 <545435711@qq.com>
2018-11-04 23:22:09 +0800

    单点登录服务增加页面

--- microservice-view/spring-cloud-provider-sso/src/main/java/acme/me/sso/action/AuthAction.java
+++ microservice-view/spring-cloud-provider-sso/src/main/java/acme/me/sso/action/AuthAction.java
@@ -2,13 +2,26 @@ package acme.me.sso.action;
 
 import acme.me.sso.rpc.ManagerServiceRpc;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.util.Date;
+
+@Controller
 @RequestMapping("auth")
 public class AuthAction {
     @Autowired
     private ManagerServiceRpc managerServiceRpc;
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index(ModelMap map) {
+        Date date = new Date();
+        map.addAttribute("vv", date.toString());
+        return "index";
+    }
 
     @RequestMapping(value = "all", method = RequestMethod.GET)
     @ResponseBody
