e368f9c18c50588b0afeecb5c7d5423b2fb64a6d
cdchenmingxuan <cdchenmingxuan@jd.com>
2018-11-16 18:08:41 +0800

    spring config

--- microservice-view/spring-cloud-config/src/main/java/acme/me/config/SpringCloudConfigApplication.java
+++ microservice-view/spring-cloud-config/src/main/java/acme/me/config/SpringCloudConfigApplication.java
@@ -3,9 +3,11 @@ package acme.me.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class SpringCloudConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigApplication.class, args);
