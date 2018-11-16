ff0b5cff2950a92abba4a88b0d3fb2da5ba55f49
cdchenmingxuan <cdchenmingxuan@jd.com>
2018-11-16 17:23:05 +0800

    增加spring boot admin

--- microservice-view/spring-cloud-eureka/src/main/java/acme/me/regiter/eureka/EurekaServerApplication.java
+++ microservice-view/spring-cloud-eureka/src/main/java/acme/me/regiter/eureka/EurekaServerApplication.java
@@ -2,10 +2,8 @@ package acme.me.regiter.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * The @SpringBootApplication annotation is equivalent to using @Configuration,
