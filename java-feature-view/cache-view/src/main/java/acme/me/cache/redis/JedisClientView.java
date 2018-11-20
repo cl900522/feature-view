a7df2775b90530035599e78141eb10665317724b
cdchenmingxuan <cdchenmingxuan@jd.com>
2018-11-20 17:44:40 +0800

    升级jedis和spring-data，支持cluster

--- java-feature-view/cache-view/src/main/java/acme/me/cache/redis/JedisClientView.java
+++ java-feature-view/cache-view/src/main/java/acme/me/cache/redis/JedisClientView.java
@@ -1,11 +1,12 @@
 package acme.me.cache.redis;
 
 import org.junit.*;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.*;
 
+import java.io.IOException;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class JedisClientView {
     private static Jedis jedis;// 非切片额客户端连接
@@ -24,6 +25,34 @@ public class JedisClientView {
 
         jedisPool = new JedisPool(config, "192.168.100.200", 6379);
         jedis = jedisPool.getResource();
+    }
+
+    public static void initCluster() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        // 最大连接数
+        poolConfig.setMaxTotal(1);
+        // 最大空闲数
+        poolConfig.setMaxIdle(1);
+        // 最大允许等待时间，如果超过这个时间还未获取到连接，则会报JedisException异常：
+        // Could not get a resource from the pool
+        poolConfig.setMaxWaitMillis(1000);
+        Set<HostAndPort> nodes = new LinkedHashSet<HostAndPort>();
+        nodes.add(new HostAndPort("192.168.83.128", 6379));
+        nodes.add(new HostAndPort("192.168.83.128", 6380));
+        nodes.add(new HostAndPort("192.168.83.128", 6381));
+        nodes.add(new HostAndPort("192.168.83.128", 6382));
+        nodes.add(new HostAndPort("192.168.83.128", 6383));
+        nodes.add(new HostAndPort("192.168.83.128", 6384));
+        JedisCluster cluster = new JedisCluster(nodes, poolConfig);
+        String name = cluster.get("name");
+        System.out.println(name);
+        cluster.set("age", "18");
+        System.out.println(cluster.get("age"));
+        try {
+            cluster.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
